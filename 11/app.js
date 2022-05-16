b7122c8155ff7e689ca5183254afb9068278b60e
seminify <seminify@gmail.com>
2022-05-16 21:00:12 +0900

    11

--- 11/app.js
+++ 11/app.js
@@ -1,33 +1,41 @@
-const url = `https://dapi.kakao.com/v2/search/web?query=#query`;
+const url = `https://dapi.kakao.com/v2/search/web?query=#query&page=#page`;
 const $docs = document.querySelector('#docs');
 const $query = document.querySelector('[name="query"]');
-const $searchButton = document.querySelector("#searchButton");
 const $searchForm = document.querySelector("#searchForm");
+const $button = document.querySelector("#button");
+let page;
 
 function getFetch(url, Callback) {
     const headers = {
         Authorization: 'KakaoAK 44a8b278d03c10f981fe99ffe524524d'
     }
-    fetch(url, { headers })
+    fetch(url, {headers})
         .then((Response) => Response.json())
         .then((data) => Callback(data));
 }
 
-function search() {
+function search(page) {
     const query = $query.value;
-    const searchUrl = url.replace('#query', query);
+    const searchUrl = url.replace('#query', query).replace('#page', page);
     getFetch(searchUrl, (data) => {
-        const { documents } = data;
+        const {documents} = data;
         console.log(documents);
         const docs = documents.map((document) => {
             console.log(document);
             return document.contents;
         });
-        $docs.innerHTML = docs.join('<hr>');
+        $docs.innerHTML += docs.join('<hr>');
     });
 }
 
 $searchForm.addEventListener('submit', (event) => {
+    page = 1;
     search();
     event.preventDefault();
+})
+
+$button.addEventListener('click',function (){
+    if(page===51) return;
+    page++;
+    search(page);
 })
